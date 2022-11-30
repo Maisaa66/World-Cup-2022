8922d48c9f229675b7b33826adfd754cbfd43d7b
Ziad Eleraky <ziadeleraky1@gmail.com>
2022-11-30 20:42:03 +0200

    Groups v1.0

---
+++ Script/main.js
@@ -0,0 +1,14 @@
+let matches = [];
+
+for (let i = 0; i < 12; i++) {
+  matches[i] = [];
+}
+
+matches[0]["TeamA"] = "Denmark";
+matches[0]["TeamB"] = "Australia";
+matches[0]["Date"] = "30-11-22";
+matches[0]["Time"] = "6:00 PM";
+matches[0]["Stadium"] = "Al Janoub Stadium";
+matches[0]["Group"] = "D";
+
+console.log(matches);
