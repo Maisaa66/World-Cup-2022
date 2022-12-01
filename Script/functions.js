a5f93312acac9308bce34c46e742e542758d6f4d
Ziad Eleraky <ziadeleraky1@gmail.com>
2022-12-01 16:20:23 +0200

    Adding JS Structures

---
+++ Script/functions.js
@@ -0,0 +1,84 @@
+function setStatus(matches) {
+  let date = new Date();
+
+  let month = date.getMonth();
+  let day = date.getDate();
+  let hour = date.getHours() - 12;
+
+  for (let i = 0; i < 12; i++) {
+    let split_date = matches[i]["Date"].split("-"); // ["2", "12" ,"22"]
+
+    let time_split = matches[i]["Time"].split(" "); // ["10:00", "PM"]
+
+    let hour_split = time_split[0].split(":"); // ["10", "00"]
+
+    // split_date[1]    ->   match month
+    // split_date[0]    ->   match day
+
+    let in_upcoming_month = split_date[1] - 1 > month;
+
+    let same_month_upcoming_day = split_date[1] - 1 == month && split_date[0] > day;
+
+    let today = split_date[1] - 1 == month && split_date[0] == day && hour < hour_split[0];
+
+    if (in_upcoming_month || same_month_upcoming_day) {
+      matches[i]["Status"] = "Next";
+    } else if (today) {
+      matches[i]["Status"] = "Today";
+    } else {
+      matches[i]["Status"] = "Already Played!";
+    }
+  }
+}
+
+function countDown(index) {
+  let date = matches[index]["Date"];
+  let time = matches[index]["Time"];
+
+  //splitting the date so i can change it from "1-12-22" to "2022-12-1"
+  let splitDate = date.split("-");
+  let newDate = "20" + splitDate[2] + "-" + splitDate[1] + "-" + splitDate[0];
+
+  // splitting the time so i can change it to 24 formate by checking it PM or AM
+  let split_time = time.split(" ");
+  let hour_min = split_time[0].split(":");
+  let hour = hour_min[0];
+  let min = hour_min[1];
+  let timeFormat = split_time[1];
+
+  if (timeFormat == "PM" && parseInt(hour) < 12) {
+    hour = parseInt(hour) + 12 + ":" + min; //hour and min in 24 formate
+  } else {
+    hour = hour + ":" + min;
+  }
+
+  // count down logic
+
+  //get the time in mellisec until the date of the match
+  let countDownDate = new Date(newDate + " " + hour).getTime();
+
+  // get the time of today
+  let now = new Date().getTime();
+
+  // get the remaining time or time left from now to the match time
+  let remain_Time = countDownDate - now;
+
+  // get the number of days, hours, min and seconds my math calculations
+  var days = Math.floor(remain_Time / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((remain_Time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((remain_Time % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((remain_Time % (1000 * 60)) / 1000);
+
+  //if the time left is 0, it means that we reach the zero hour, so we clear the interval
+  if (remain_Time < 0) {
+    clearInterval(myCountDownInterval);
+    document.getElementById("counter").innerText = "The Game Is Starting NOW!!!";
+    console.log("HII");
+  } else {
+    // if there is still time left show it in the page
+    document.getElementById("counter").innerText = days + " Day : " + hours + " Hour : " + minutes + " Min: " + seconds + " Sec";
+  }
+
+  console.log(hour, remain_Time);
+  console.log("%c" + days + ":" + hours + ":" + minutes + ":" + seconds, "font-size:45px");
+}
---
+++ Script/matchesInfo.js
@@ -0,0 +1,101 @@
+let matches = [];
+
+for (let i = 0; i < 12; i++) {
+  matches[i] = [];
+}
+
+matches[0]["TeamA"] = "Denmark";
+matches[0]["TeamB"] = "Australia";
+matches[0]["Date"] = "30-11-22";
+matches[0]["Time"] = "6:00 PM";
+matches[0]["Stadium"] = "Al Janoub Stadium";
+matches[0]["Group"] = "D";
+matches[0]["Status"];
+
+matches[1]["TeamA"] = "Tunisia";
+matches[1]["TeamB"] = "France";
+matches[1]["Date"] = "30-11-22";
+matches[1]["Time"] = "6:00 PM";
+matches[1]["Stadium"] = "Education City Stadium";
+matches[1]["Group"] = "D";
+matches[1]["Status"];
+
+matches[2]["TeamA"] = "Poland";
+matches[2]["TeamB"] = "Argentina";
+matches[2]["Date"] = "30-11-22";
+matches[2]["Time"] = "10:00 PM";
+matches[2]["Stadium"] = "Stadium 974";
+matches[2]["Group"] = "C";
+matches[2]["Status"];
+
+matches[3]["TeamA"] = "Saudi Arabia";
+matches[3]["TeamB"] = "Mexico";
+matches[3]["Date"] = "30-11-22";
+matches[3]["Time"] = "10:00 PM";
+matches[3]["Stadium"] = "Lusail Stadium";
+matches[3]["Group"] = "C";
+matches[3]["Status"];
+
+matches[4]["TeamA"] = "Croatia";
+matches[4]["TeamB"] = "Belgium";
+matches[4]["Date"] = "1-12-22";
+matches[4]["Time"] = "6:00 PM";
+matches[4]["Stadium"] = "Ahmad Bin Ali Stadium";
+matches[4]["Group"] = "F";
+matches[4]["Status"];
+
+matches[5]["TeamA"] = "Canada";
+matches[5]["TeamB"] = "Morocco";
+matches[5]["Date"] = "1-12-22";
+matches[5]["Time"] = "6:00 PM";
+matches[5]["Stadium"] = "Al Thumama Stadium";
+matches[5]["Group"] = "F";
+matches[5]["Status"];
+
+matches[6]["TeamA"] = "Japan";
+matches[6]["TeamB"] = "Spain";
+matches[6]["Date"] = "1-12-22";
+matches[6]["Time"] = "10:00 PM";
+matches[6]["Stadium"] = "Khalifa International Stadium";
+matches[6]["Group"] = "E";
+matches[6]["Status"];
+
+matches[7]["TeamA"] = "Costa Rica";
+matches[7]["TeamB"] = "Germany";
+matches[7]["Date"] = "1-12-22";
+matches[7]["Time"] = "10:00 PM";
+matches[7]["Stadium"] = "Al Bayt Stadium";
+matches[7]["Group"] = "E";
+matches[7]["Status"];
+
+matches[8]["TeamA"] = "Ghana";
+matches[8]["TeamB"] = "Uruguay";
+matches[8]["Date"] = "2-12-22";
+matches[8]["Time"] = "6:00 PM";
+matches[8]["Stadium"] = "Al Janoub Stadium";
+matches[8]["Group"] = "H";
+matches[8]["Status"];
+
+matches[9]["TeamA"] = "South Korea";
+matches[9]["TeamB"] = "Portugal";
+matches[9]["Date"] = "2-12-22";
+matches[9]["Time"] = "6:00 PM";
+matches[9]["Stadium"] = "Education City Stadium";
+matches[9]["Group"] = "H";
+matches[9]["Status"];
+
+matches[10]["TeamA"] = "Serbia";
+matches[10]["TeamB"] = "Switzerland";
+matches[10]["Date"] = "2-12-22";
+matches[10]["Time"] = "10:00 PM";
+matches[10]["Stadium"] = "Stadium 974";
+matches[10]["Group"] = "G";
+matches[10]["Status"];
+
+matches[11]["TeamA"] = "Brazil";
+matches[11]["TeamB"] = "Cameroon";
+matches[11]["Date"] = "2-12-22";
+matches[11]["Time"] = "10:00 PM";
+matches[11]["Stadium"] = "Lusail Stadium";
+matches[11]["Group"] = "G";
+matches[11]["Status"];
