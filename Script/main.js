d42aee92fc50fd8f954ea72ffab70697b6933756
Maisaa Hamza <maisaa.hamza66@gmail.com>
2022-12-01 02:32:30 +0200

    add some comments
    comments at the count down clock function

--- Script/main.js
+++ Script/main.js
@@ -152,9 +152,11 @@ function countDown(index) {
   let date = matches[index]["Date"];
   let time = matches[index]["Time"];
 
+  //splitting the date so i can change it from "1-12-22" to "2022-12-1"
   let splitDate = date.split("-");
   let newDate = "20" + splitDate[2] + "-" + splitDate[1] + "-" + splitDate[0];
 
+  // splitting the time so i can change it to 24 formate by checking it PM or AM
   let split_time = time.split(" ");
   let hour_min = split_time[0].split(":");
   let hour = hour_min[0];
@@ -168,21 +170,30 @@ function countDown(index) {
     hour=hour+":"+min;
   }
 
+  // count down logic
+
+  //get the time in mellisec until the date of the match
   let countDownDate = new Date(newDate + " " + hour).getTime();
+
+  // get the time of today
   let now = new Date().getTime();
 
+  // get the remaining time or time left from now to the match time
   let remain_Time = countDownDate - now;
 
+  // get the number of days, hours, min and seconds my math calculations
   var days = Math.floor(remain_Time / (1000 * 60 * 60 * 24));
   var hours = Math.floor((remain_Time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((remain_Time % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((remain_Time % (1000 * 60)) / 1000);
 
+  //if the time left is 0, it means that we reach the zero hour, so we clear the interval
   if (remain_Time < 0) {
     clearInterval(myCountDownInterval);
     document.getElementById("counter").innerText = "The Game Is Starting NOW!!!";
     console.log("HII")
 }else{
+  // if there is still time left show it in the page
   document.getElementById("counter").innerText= days+" Day : "+hours+" Hour : "+minutes+" Min: "+ seconds+" Sec";
 
 }
@@ -191,6 +202,7 @@ console.log(hour, remain_Time)
  console.log("%c"+days+":"+hours+":"+minutes+":"+ seconds, "font-size:45px");
 }
 
+// setinterval to update the timer each 1 sec
 let myCountDownInterval = setInterval(function(){
   countDown(12);
 }, 1000);
