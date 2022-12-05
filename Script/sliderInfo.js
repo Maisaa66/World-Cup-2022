b47b7537a295ed2239954c52b2cfeabcae926d78
unknown <yara_elgazar@outlook.com>
2022-12-05 21:40:24 +0200

    Slider modification

--- Script/sliderInfo.js
+++ Script/sliderInfo.js
@@ -6,85 +6,85 @@ for (let i = 0; i < 14; i++) {
 
 // 1
 bestGoals[0]["year"] = "2018";
-bestGoals[0]["url"] = "https://www.fifa.com/fifaplus/en/watch/2M4ylDZw7S67LKHySHV0bN";
-bestGoals[0]["img"] =
-  "https://digitalhub.fifa.com/transform/465dac29-e94b-404e-bc34-662ff9c63b51/2018_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[0]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/2M4ylDZw7S67LKHySHV0bN";
+bestGoals[0]["img"] = "../Images/slider/2018.webp";
 bestGoals[0]["caption"] = "Best Goals | 2018 FIFA World Cup Russia™";
 // 2
 bestGoals[1]["year"] = "2014";
-bestGoals[1]["url"] = "https://www.fifa.com/fifaplus/en/watch/4QAa7NSW2saspfbew8KA2S";
-bestGoals[1]["img"] =
-  "https://digitalhub.fifa.com/transform/5831674b-1115-4ace-b173-2c31a19e9644/2014_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[1]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/4QAa7NSW2saspfbew8KA2S";
+bestGoals[1]["img"] = "../Images/slider/2014.webp";
 bestGoals[1]["caption"] = "Best Goals | 2014 FIFA World Cup Brazil™";
 // 3
 bestGoals[2]["year"] = "2010";
-bestGoals[2]["url"] = "https://www.fifa.com/fifaplus/en/watch/7DyUeM18UKvOmC3TlkRj5O";
-bestGoals[2]["img"] =
-  "https://digitalhub.fifa.com/transform/41e6026e-071c-4aca-82f3-76cbd6d55699/2010_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[2]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/7DyUeM18UKvOmC3TlkRj5O";
+bestGoals[2]["img"] = "../Images/slider/2010.webp";
 bestGoals[2]["caption"] = "Best Goals | 2010 FIFA World Cup South Africa™";
 // 4
 bestGoals[3]["year"] = "2006";
-bestGoals[3]["url"] = "https://www.fifa.com/fifaplus/en/watch/2b8xHcvqUzKqyugOHIdFh2";
-bestGoals[3]["img"] =
-  "https://digitalhub.fifa.com/transform/cc3063f5-1e0c-4e91-ba97-4c577e173bea/2006_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[3]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/2b8xHcvqUzKqyugOHIdFh2";
+bestGoals[3]["img"] = "../Images/slider/2006.webp";
 bestGoals[3]["caption"] = "Best Goals | 2006 FIFA World Cup Germany™";
 // 5
 bestGoals[4]["year"] = "2002";
-bestGoals[4]["url"] = "https://www.fifa.com/fifaplus/en/watch/62UySq7D7gf9siFimQb308";
-bestGoals[4]["img"] =
-  "https://digitalhub.fifa.com/transform/cdb65512-4dfe-46c6-ab20-01af98091cf9/2002_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[4]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/62UySq7D7gf9siFimQb308";
+bestGoals[4]["img"] = "../Images/slider/2002.webp";
 bestGoals[4]["caption"] = "Best Goals | 2002 FIFA World Cup Korea/Japan™";
 // 6
 bestGoals[5]["year"] = "1998";
-bestGoals[5]["url"] = "https://www.fifa.com/fifaplus/en/watch/1ZF2uaZqAjmbvvvM0JoFaH";
-bestGoals[5]["img"] =
-  "https://digitalhub.fifa.com/transform/caa60b46-3b61-47cf-9d60-5eed281fa7e8/1998_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[5]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/1ZF2uaZqAjmbvvvM0JoFaH";
+bestGoals[5]["img"] = "../Images/slider/1998.webp";
 bestGoals[5]["caption"] = "Best Goals | 1998 FIFA World Cup France™";
 // 7
 bestGoals[6]["year"] = "1994";
-bestGoals[6]["url"] = "https://www.fifa.com/fifaplus/en/watch/6E6HKBAH8NN8ZAVVU1Wc2I";
-bestGoals[6]["img"] =
-  "https://digitalhub.fifa.com/transform/bbfec0bb-8450-4051-bc95-35b549926c95/1994_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[6]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/6E6HKBAH8NN8ZAVVU1Wc2I";
+bestGoals[6]["img"] = "../Images/slider/1994.webp";
 bestGoals[6]["caption"] = "Best Goals | 1994 FIFA World Cup USA™";
 // 8
 bestGoals[7]["year"] = "1990";
-bestGoals[7]["url"] = "https://www.fifa.com/fifaplus/en/watch/HazDQaVHWWCOC4MvadQuV";
-bestGoals[7]["img"] =
-  "https://digitalhub.fifa.com/transform/2c9fee73-761e-40ef-8b81-52332c05f87d/1990_Best_Goals_Portrait-jpjg?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[7]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/HazDQaVHWWCOC4MvadQuV";
+bestGoals[7]["img"] = "../Images/slider/1990.webp";
 bestGoals[7]["caption"] = "Best Goals | 1990 FIFA World Cup Italy™";
 // 9
 bestGoals[8]["year"] = "1986";
-bestGoals[8]["url"] = "https://www.fifa.com/fifaplus/en/watch/6A8PBblf9CUJZsU3q8EWQT";
-bestGoals[8]["img"] =
-  "https://digitalhub.fifa.com/transform/dff25282-c966-475f-b615-108e98e810b3/1986_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[8]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/6A8PBblf9CUJZsU3q8EWQT";
+bestGoals[8]["img"] = "../Images/slider/1986.webp";
 bestGoals[8]["caption"] = "Best Goals | 1986 FIFA World Cup Mexico™";
 // 10
 bestGoals[9]["year"] = "1982";
-bestGoals[9]["url"] = "https://www.fifa.com/fifaplus/en/watch/49gamflD3KdCdXF5TKhAYd";
-bestGoals[9]["img"] =
-  "https://digitalhub.fifa.com/transform/4c3d7f34-3fa1-453a-86e5-7a175c432036/1982_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[9]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/49gamflD3KdCdXF5TKhAYd";
+bestGoals[9]["img"] = "../Images/slider/1982.webp";
 bestGoals[9]["caption"] = "Best Goals | 1982 FIFA World Cup Spain™";
 // 11
 bestGoals[10]["year"] = "1978";
-bestGoals[10]["url"] = "https://www.fifa.com/fifaplus/en/watch/3cKBBZ63gVuArZHAQcTK3o";
-bestGoals[10]["img"] =
-  "https://digitalhub.fifa.com/transform/408d454e-b852-4ba0-991f-966bfe1b47c8/1978_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[10]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/3cKBBZ63gVuArZHAQcTK3o";
+bestGoals[10]["img"] = "../Images/slider/1978.webp";
 bestGoals[10]["caption"] = "Best Goals | 1978 FIFA World Cup Argentina™";
 // 12
 bestGoals[11]["year"] = "1974";
-bestGoals[11]["url"] = "https://www.fifa.com/fifaplus/en/watch/2k8iyWI0m1VbPheYXAhT8c";
-bestGoals[11]["img"] =
-  "https://digitalhub.fifa.com/transform/d0d646dc-250c-48d8-9a8f-78719856a822/1974_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[11]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/2k8iyWI0m1VbPheYXAhT8c";
+bestGoals[11]["img"] = "../Images/slider/1974.webp";
 bestGoals[11]["caption"] = "Best Goals | 1974 FIFA World Cup Germany™";
 // 13
 bestGoals[12]["year"] = "1970";
-bestGoals[12]["url"] = "https://www.fifa.com/fifaplus/en/watch/563BAs9TXQWjonPGxXo9su";
-bestGoals[12]["img"] =
-  "https://digitalhub.fifa.com/transform/b1b9bc82-7137-42a7-bcea-014be9fb064b/1970_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[12]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/563BAs9TXQWjonPGxXo9su";
+bestGoals[12]["img"] = "../Images/slider/1970.webp";
 bestGoals[12]["caption"] = "Best Goals | 1970 FIFA World Cup Mexico™";
 // 14
 bestGoals[13]["year"] = "1966";
-bestGoals[13]["url"] = "https://www.fifa.com/fifaplus/en/watch/O2Qyt2FrAHhuXSM5wCUhF";
-bestGoals[13]["img"] =
-  "https://digitalhub.fifa.com/transform/d423bc4d-b4c1-4010-aa57-50bffec5d269/1966_Best_Goals_Portrait?io=transform:fill,aspectratio:10x16&quality=100";
+bestGoals[13]["url"] =
+  "https://www.fifa.com/fifaplus/en/watch/O2Qyt2FrAHhuXSM5wCUhF";
+bestGoals[13]["img"] = "../Images/slider/1966.webp";
 bestGoals[13]["caption"] = "Best Goals | 1966 FIFA World Cup England™";
